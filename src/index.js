098926b7f8aa50aaef192586fd812357a20db104
haleybarlar <hbarlar1@gmail.com>
2018-09-28 11:11:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const yourUUID = 1 //Enter your assigned uuid here
-
+  const yourUUID = 'b374cace-6a95-43fe-829e-1bb4d0f87db6'
   const imageURL = `https://randopic.herokuapp.com/images/${yourUUID}`
-  
-  let imageId = 1 //Enter the id from the fetched image here
-
+  let imageId = 882
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const container = document.querySelector(".container")
 
+  function fetchAll() {
+    return fetch(imageURL)
+    .then(res => res.json())
+  }
 
+  function renderThem() {
+    let information = fetchAll()
+    information.then(resp => {
+      let imageCard = document.querySelector("#image-card")
+
+      let src = document.querySelector("#image")
+      image.src = `https://randopic.herokuapp.com/images/${yourUUID}`
+      src.appendChild(image)
+
+      let name = document.querySelector("#name")
+      name.innerText = resp.name
+
+      let likes = document.querySelector("#likes")
+      likes.innerText = resp.like_count
+
+      // comments = document.querySelector("#comments")
+      // comments.innerText = information.comments[0].content
+
+      imageCard.appendChild(image, name, likes, comments)
+      container.append(imageCard)
+    })
+  }
+  //
+  renderThem()
 
 })
